a759d21cbca394c583aa05c7b8142abe64163076
Pannaton Krodsen <komop9988@gmail.com>
2026-02-27 03:14:43 +0700

    Employee Form Project

---
+++ myapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("<h1>หน้าแรกของเว็บไซต์วิชา ICT12367</h1>")
+
+def about(request):
+    return HttpResponse("<h1>เกี่ยวกับเรา</h1>")
+
+
+def form(request):
+    return render(request, 'form.html')
+
+def contact(request):
+    return HttpResponse("<h1>ติดต่อ</h1><p>รหัสนักศึกษา: 68017981</p><p>ชื่อ นามสกุล: ปัญณธร กรดเสน</p>")
